ef463253666d4ae3d8b840261e59a8abc7a3cbf7
Toothpaste-squasher <kwanho.kh.chan@gmail.com>
2025-08-05 11:34:18 +1000

    Add files via upload

---
+++ Scripts/data/priorities.js
@@ -0,0 +1,17 @@
+export const priority = [
+  {
+    label: "high",
+    color: "rgba(255, 134, 134, 1)",
+    sortOrder: 1
+  },
+  {
+    label: "medium",
+    color: "rgba(121, 235, 255, 1)",
+    sortOrder: 2
+  },
+  {
+    label: "low",
+    color: "rgba(179, 255, 121, 1)",
+    sortOrder: 3
+  }
+];
